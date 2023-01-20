4a535c9336c0a8a50421aaf45281374f633ef918
JakubBrzozowski97 <51862197+JakubBrzozowski97@users.noreply.github.com>
2023-01-20 15:48:15 -0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
 import java.util.*;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
+import Cassandra.BackendException;
+import Cassandra.BackendSession;
 import java.io.IOException;
 import java.util.Properties;
 import com.datastax.driver.core.Row;
@@ -13,71 +17,52 @@ import com.datastax.oss.driver.api.core.type.DataTypes;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateTable;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateTableStart;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateTableWithOptions;
-
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import java.util.UUID;
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import static com.datastax.driver.core.DataType.uuid;
 import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.lang.Integer.toOctalString;
 import static org.junit.Assert.*;
 
 public class Main {
+    public static void main(String[] args) throws BackendException{
 
-    private static final String PROPERTIES_FILENAME = "config.properties";
-    public static void main(String[] args) throws IOException{
-        String contactPoint = null;
-        String keyspace = null;
-
-        Scanner input = new Scanner(System.in);
-
-        Cluster cluster;
-        Session session;
-
-        Properties properties = new Properties();
-
-        try {
-            properties.load(Main.class.getClassLoader().getResourceAsStream(PROPERTIES_FILENAME));
-
-            contactPoint = properties.getProperty("contact_point");
-            keyspace = properties.getProperty("keyspace");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        cluster = Cluster.builder().addContactPoint(contactPoint).build();
-        session = cluster.connect(keyspace);
+        BackendSession backend = new BackendSession("config.properties");
 
 
-        /*StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXISTS ")
-                .append("coss").append("(")
-                .append("id uuid PRIMARY KEY, ")
-                .append("title text,")
-                .append("subject text);");
 
-        String query = sb.toString();
-        session.execute(query);*/
 
-        StringBuilder sb1 =new StringBuilder("INSERT INTO ")
-                .append("coss ")
-                .append("(id, title, subject) ")
-                .append("VALUES ")
-                .append("(uuid(), 'sraki', 'pierdziaki');");
+        //Tickets ticks = new Tickets(backend);
+        //ticks.addTicket(4,4,4,4);
+        // System.out.println(ticks.getTickets());
+        // ticks.deleteTicket(4);
+        // System.out.println(ticks.getTickets());
+        Tickets_Company tc = new Tickets_Company(backend);
+        Ticket tics = new Ticket(backend);
+        tics.addTicket(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID(), 1);
+        Client client = new Client(backend);
+        Place place = new Place(backend);
+        place.addPlace(UUID.randomUUID(),"A");
+        Event event = new Event(backend);
+        Timestamp ts1 = Timestamp.valueOf("2018-09-01 09:01:15");
+        Timestamp ts2 = Timestamp.valueOf("2018-09-01 10:01:15");
+        event.addEvent(UUID.randomUUID(), "MECZyk","MECZ",ts1, ts2, 1);
+        tc.addTickets_Company(1, "Ticketmaster", "473473667","Poznan");
+        //Credit_card cc = new Credit_card(backend);
+        //cc.addCredit_card(ClientID, Credit_cardsID, "NULL");
+        //UUID ClientID = UUID.randomUUID();
+        client.addClient();
+        //System.out.println(client.getClients());
+        //System.out.println(ClientID);
 
-        String query1 = sb1.toString();
-        session.execute(query1);
 
-/*
-        StringBuilder sb1 = new StringBuilder("DELETE FROM ")
-                .append("kejspejs.coss ")
-                .append("WHERE id= ")
-                .append("078d0315-97d7-41c0-97c9-c9472a7f424d ")
-                .append("IF EXISTS;");
 
-        String query1 = sb1.toString();
-        session.execute(query1);
-*/
-
-        session.close();
-        cluster.close();
     }
 
 }
